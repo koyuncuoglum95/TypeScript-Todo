489ee152aa1067b4b970d820d8d156e81046589c
koyuncuoglum95 <77508734+koyuncuoglum95@users.noreply.github.com>
2022-03-05 22:14:55 -0800

    typescript todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Todos, NewTodo } from './components/index';
+import { TodosContextProvider } from './store/todos-context';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodosContextProvider>
+        <Todos />
+        <NewTodo />
+      </TodosContextProvider>
     </div>
   );
 }
